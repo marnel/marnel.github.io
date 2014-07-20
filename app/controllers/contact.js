b4f97621eb7a76bedd84a6b498e020997e500cf3
marnel <michael.arnel@gmail.com>
2014-07-20 19:22:13 -0400

    add contact controller

---
+++ app/controllers/contact.js
@@ -0,0 +1,12 @@
+/**
+ * Created by marnel on 7/20/14.
+ */
+
+angular.module('myApp').controller('contactController', ['$scope',
+    function($scope) {
+
+        $scope.contact = function(){
+            alert('Coming Soon!');
+        };
+    }
+]);
