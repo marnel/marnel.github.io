3d105ca3c6958ae8575e25c42b4167198fa5aa19
Michael Arnel <michael.arnel@gmail.com>
2015-02-21 14:42:31 -0500

    removed old app folder

--- app/SiteDataService.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by marnel on 5/18/14.
- */
-
-angular.module('myApp.Services',[]).factory('siteDataService',
-    function($resource){
-        return $resource('../../site.json');
-    }
-);
--- app/controllers/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('myApp').controller('IndexController', ['$scope', function ($scope) {
-
-}]);
